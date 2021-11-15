5e75041610ccbf42a92bde4b5ed7b7f8b4be124a
Eivind Holt <eivholt@gmail.com>
2021-11-15 22:07:49 +0100

    Update decoder to The Things Stack v3

--- MiniUltraLoRaWAN/The Things Network Application/PayloadDecoder.js
+++ MiniUltraLoRaWAN/The Things Network Application/PayloadDecoder.js
@@ -1,10 +1,14 @@
-function Decoder(b, port) {
-
-    var d = {};
-      d.battery = (b[0]<<8 | b[1]);
-      d.capacitance1 = ((b[2] & 0x80 ? 0xFFFF<<16 : 0) | b[2]<<8 | b[3]);
-      d.tempc1 = ((b[4] & 0x80 ? 0xFFFF<<16 : 0) | b[4]<<8 | b[5]) / 10.0;
-      d.capacitance2 = ((b[6] & 0x80 ? 0xFFFF<<16 : 0) | b[6]<<8 | b[7]);
-      d.capacitance3 = ((b[8] & 0x80 ? 0xFFFF<<16 : 0) | b[8]<<8 | b[9]);
-      return d;
+function decodeUplink(input) {
+    var bytes = input.bytes
+    return {
+      data: {
+        battery : (bytes[0]<<8 | bytes[1]),
+        capacitance1 : ((bytes[2] & 0x80 ? 0xFFFF<<16 : 0) | bytes[2]<<8 | bytes[3]),
+        tempc1 : ((bytes[4] & 0x80 ? 0xFFFF<<16 : 0) | bytes[4]<<8 | bytes[5]) / 10.0,
+        capacitance2 : ((bytes[6] & 0x80 ? 0xFFFF<<16 : 0) | bytes[6]<<8 | bytes[7]),
+        capacitance3 : ((bytes[8] & 0x80 ? 0xFFFF<<16 : 0) | bytes[8]<<8 | bytes[9]),
+      },
+      warnings: [],
+      errors: []
+    };
   }
